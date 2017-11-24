8de258e09609ee3ff9c60bf7f389bdb4d45fa1aa
Grant <522852537@qq.com>
2017-11-24 17:14:18 +0800

    更新蓝牙实现

---
+++ BluetoothConnection/app/src/main/java/com/bluetooth/connection/BlueStatus.java
@@ -0,0 +1,11 @@
+package com.bluetooth.connection;
+
+/**
+ * Created by taro on 2017/7/6.
+ */
+
+public class BlueStatus {
+    public String addr;
+    public String name;
+    public boolean isConnected;
+}
